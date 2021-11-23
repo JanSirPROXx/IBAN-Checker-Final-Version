f9b6463a419ef57327893d35e49cbfc04e2721b3
Jan <84132136+JanSirPROXx@users.noreply.github.com>
2021-11-23 19:46:51 +0100

    Add files via upload
    v1

---
+++ IBAN Checker/MainForm.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace IBAN_Checker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label_checked = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label_pruef = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(130, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(299, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IBAN Checker";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "IBAN:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(130, 136);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(299, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(130, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 28);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Check IBAN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Converted IBAN:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(302, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Credits: Jan, Josua";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "State:";
+            // 
+            // label_checked
+            // 
+            this.label_checked.AutoSize = true;
+            this.label_checked.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label_checked.Location = new System.Drawing.Point(93, 198);
+            this.label_checked.Name = "label_checked";
+            this.label_checked.Size = new System.Drawing.Size(77, 17);
+            this.label_checked.TabIndex = 8;
+            this.label_checked.Text = "unchecked";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(137, 57);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(217, 17);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "XXXX XXXX XXXX XXXX XXXX X";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(10, 181);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(77, 17);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Pruefziffer:";
+            // 
+            // label_pruef
+            // 
+            this.label_pruef.AutoSize = true;
+            this.label_pruef.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label_pruef.Location = new System.Drawing.Point(93, 181);
+            this.label_pruef.Name = "label_pruef";
+            this.label_pruef.Size = new System.Drawing.Size(28, 17);
+            this.label_pruef.TabIndex = 11;
+            this.label_pruef.Text = "----";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 224);
+            this.Controls.Add(this.label_pruef);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label_checked);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label_checked;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label_pruef;
+    }
+}
+
---
+++ IBAN Checker/MainForm.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+//Using BigInt
+//using BigMath;
+using System.Numerics;
+
+
+//Credits to Jan and Josua
+namespace IBAN_Checker
+{
+    public partial class MainForm : Form
+    {
+
+        string IBAN;
+
+        public object IBAN_U { get; private set; }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IBAN = textBox1.Text;
+
+            string[] IBAN_L = IBAN.Split(' ');
+            //Für die Prüfziffer
+            string Pruef1 = IBAN_L[0].Insert(2, " ");
+            string[] Pruef2 = Pruef1.Split(' ');
+            string Pruefziffer = Pruef2[1];
+            string CH_ding = Pruef2[0];
+            
+
+            string IBAN_N = IBAN_L[1] + IBAN_L[2] + IBAN_L[3] + IBAN_L[4] + IBAN[5] + CH_ding + "00"; 
+
+            //MessageBox.Show(IBAN_N);
+
+
+            
+            //Replace Others
+            IBAN_N = IBAN_N.Replace(" ", "");
+
+            //Replace Chars
+            IBAN_N = IBAN_N.Replace("A", "10");
+            IBAN_N = IBAN_N.Replace("B", "11");
+            IBAN_N = IBAN_N.Replace("C", "12");
+            IBAN_N = IBAN_N.Replace("D", "13");
+            IBAN_N = IBAN_N.Replace("E", "14");
+            IBAN_N = IBAN_N.Replace("F", "15");
+            IBAN_N = IBAN_N.Replace("G", "16");
+            IBAN_N = IBAN_N.Replace("H", "17");
+            IBAN_N = IBAN_N.Replace("I", "18");
+            IBAN_N = IBAN_N.Replace("J", "19");
+            IBAN_N = IBAN_N.Replace("K", "20");
+            IBAN_N = IBAN_N.Replace("L", "21");
+            IBAN_N = IBAN_N.Replace("M", "22");
+            IBAN_N = IBAN_N.Replace("N", "23");
+            IBAN_N = IBAN_N.Replace("O", "24");
+            IBAN_N = IBAN_N.Replace("P", "25");
+            IBAN_N = IBAN_N.Replace("Q", "26");
+            IBAN_N = IBAN_N.Replace("R", "27");
+            IBAN_N = IBAN_N.Replace("S", "28");
+            IBAN_N = IBAN_N.Replace("T", "29");
+            IBAN_N = IBAN_N.Replace("U", "30");
+            IBAN_N = IBAN_N.Replace("V", "31");
+            IBAN_N = IBAN_N.Replace("W", "32");
+            IBAN_N = IBAN_N.Replace("X", "33");
+            IBAN_N = IBAN_N.Replace("Y", "34");
+            IBAN_N = IBAN_N.Replace("Z", "35");
+            //...
+
+
+            textBox2.Text = IBAN_N;
+
+            
+            int PrfZ = int.Parse(Pruefziffer);
+            BigInteger IBAN_Big1 = BigInteger.Parse(IBAN_N);
+            BigInteger IBAN_Big2 = IBAN_Big1 % 97;
+            BigInteger IBAN_Big = 98 - IBAN_Big2;
+
+            //Load Date to GUI
+            label_pruef.Text = Pruefziffer;
+            if(PrfZ == IBAN_Big)
+            {
+                label_checked.ForeColor = Color.Green;
+                label_checked.Text = "Valid";
+            }
+            else
+            {
+                label_checked.ForeColor = Color.Red;
+                label_checked.Text = "Invalid";
+            }
+            
+            
+ 
+
+            
+            
+            //textBox2.Text = IBAN_B.ToString();
+
+        }
+    }
+}
